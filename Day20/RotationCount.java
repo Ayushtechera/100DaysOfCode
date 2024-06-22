49deef0697aa9117f8b12b7a2f7a4377bb098d24
Ayushtechera <144578026+Ayushtechera@users.noreply.github.com>
2024-06-23 00:46:09 +0530

    Add files via upload

--- Day20/RotationCount.java
+++ Day20/RotationCount.java
@@ -6,38 +6,37 @@ public class RotationCount {
 
     public static int countRotation(int[] arr){
         int pivot = FindPivot(arr);
-        if(pivot == -1){
-            return 0;
-        }
-        return pivot + 1;
+        // if(pivot == -1){
+        //     return 0;
+        // } //we can remove this if condition because, when the value of pivot is 
+        return pivot + 1;  // -1 and in return it will become -1 + 1 = 0 already hence we can remove the if condition
     }
 
     // Use this for non duplicates elements in array
     public static int FindPivot(int[] arr){
         int start = 0;
-        int end = arr.length-1;
+        int end =  arr.length-1;
+
         while(start <= end){
             int mid = start + (end - start)/2;
-            // There will be 4 cases (Expalanation in Notes)
-            //CASE-1
-            if(mid < end && arr[mid] > arr[mid+1]){  // we have to keep in mind that when we do mid + 1 then the array should not be out of bound.What if suppose mid is laste element and when it will check for mid+1 then it will array out of bound.So, the mid should always be less than end whenever it is checking for the mid + 1.
+
+            // 4 CASES will be:
+            if(arr[mid] > arr[mid+1]){
                 return mid;
             }
-            //CASE-2
-            if(mid > start && arr[mid] < arr[mid - 1]){ // Similarly mid should always > start
-                return mid -1;
+            if(arr[mid] < arr[mid - 1]){
+                return mid - 1;
             }
-            //CASE-3
             if(arr[start] >= arr[mid]){
-                end = mid - 1;
+                end = mid -1;
             }
-            //CASE-4
             if(arr[start] < arr[mid]){
-                start = mid + 1;
+                start =  mid + 1;
             }
         }
-        return -1; // When pivot is not found in the array then return -1.
+        return -1;
     }
+    
 
 
     // use this when array contains duplicates
