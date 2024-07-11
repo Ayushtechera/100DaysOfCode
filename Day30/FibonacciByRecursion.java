4eec5c90f5530722544d1f49d1463410e86bdaff
Ayush Kashyap <144578026+Ayushtechera@users.noreply.github.com>
2024-07-11 23:43:42 +0530

    Add files via upload

---
+++ Day30/FibonacciByRecursion.java
@@ -0,0 +1,19 @@
+public class FibonacciByRecursion {
+    public static void main(String[] args) {
+        // int ans = fibo(4);
+        // System.out.println(ans);
+        System.out.println(fibo(4));
+        
+    }
+
+    static int fibo(int n){
+        // The base conditon:
+        if(n < 2){
+            return n;
+
+        }
+        // Recursive call
+        // this is not the tail recursion because it will first take the fibo(n-1) then it will find out the fibo(n-2) then it will add both the values and return it.
+        return fibo(n-1) + fibo(n-2);
+    }
+}
